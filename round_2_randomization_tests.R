5f8708bde33a5a4bbb7fd5eb465ba0e6a886b4bf
Michael Chirico <MichaelChirico4@gmail.com>
2015-08-09 21:15:31 -0400

    Closes #3; plots attached to Google Doc

--- round_2_randomization_tests.R
+++ round_2_randomization_tests.R
@@ -5,7 +5,7 @@
 # Setup: Packages, Working Directory, Convenient Functions ####
 rm(list=ls(all=T))
 gc()
-setwd("~/Desktop/research/Sieg_LMI_Real_Estate_Delinquency/round_two")
+setwd("~/Desktop/research/Sieg_LMI_Real_Estate_Delinquency")
 library(data.table)
 library(xlsx)
 library(maptools)
@@ -22,125 +22,64 @@ dev.off2<-function(){
   invisible(dev.off(which=dev.list()["pdf"]))
 }
 
-# Data import
+# Data import ####
 
-data<-fread("round_2_full_data.csv")
+data<-fread("./round_two/round_2_full_data.csv")
 
 ## Add shorthand for each treatment
 data[,tmt:=paste0(substr(treatment,1,1),
-                  ifelse(grepl("Big",treatment),"B","S"))]
+                  ifelse(grepl("Big",treatment),"B","S"))
+     ][,bs:=gsub(".*(?=(Big)|(Small))|(?<=(Big)|(Small)).*",
+                 "",treatment,perl=T)
+       ][,tmt7:=gsub("_.*","",treatment)]
+
+data_own<-data[,.(tmt=tmt[1],bs=bs[1],tmt7=tmt7[1],
+                  total_due=sum(total_due)),by=owner1]
 
 # Graphic Tests
-rand_path<-"./images/randomization_tests/"
-## Bar Plot: (Log) Dollar Value of Debt by Treatment
-pdf2(paste0(rand_path,"avg_due_by_trt_bar.pdf"))
-data[,mean(log(total_due)),by=tmt
-     ][,barplot(V1,names.arg=tmt,
-                main="Average Log Due by Treatment",ylab="Log $",las=3,
-                col=rep(c("yellow","blue","darkgreen","red",
-                          "cyan","orange","orchid"),each=2))]
-dev.off2()
-
-## Box Plot: Dollar Value of Debt by Treatment
-### First, 14 Treatments
-pdf2(paste0(rand_path,"dist_due_by_trt_14_box.pdf"))
-boxplot(log(total_due)~tmt,data=data,xlab="Log $",
-        main="Box Plots of Log Debt\nBy Treatment",
-        col=rep(c("yellow","blue","darkgreen","red",
-                  "cyan","orange","orchid"),each=2),
-        horizontal=T,las=1,notch=T)
-dev.off2()
-### Next, 7 Treatments (Eliminate Big/Small Distinction)
-pdf2(paste0(rand_path,"dist_due_by_trt_7_box.pdf"))
-boxplot(log(total_due)~substr(tmt,1,1),data=data,
+rand_path<-"./papers_presentations/round_two/images/balance/"
+## Log Balance by Letter
+pdf2(paste0(rand_path,"dist_log_due_by_trt_7_box.pdf"))
+par(mar=c(7.1,6.1,6.1,2.1))
+boxplot(log10(total_due)~tmt7,data=data_own,
         main="Box Plots of Log Debt\nBy Treatment",
         col=c("yellow","blue","darkgreen","red",
               "cyan","orange","orchid"),notch=T,
-        horizontal=T,las=1,xlab="Log $")
-dev.off2()
-### Finally, 14 Treatments (Unique Owner)
-pdf2(paste0(rand_path,"dist_due_by_trt_14_unq_own_box.pdf"))
-boxplot(log(total_due)~tmt,
-        data=data[opa_no %in% data[,if (.N==1) opa_no,by=owner1]$V1],
-        xlab="Log $",
-        main="Box Plots of Log Debt\nBy Treatment, Unique Owners",
-        col=rep(c("yellow","blue","darkgreen","red",
-                  "cyan","orange","orchid"),each=2),
-        horizontal=T,las=1,notch=T)
+        boxwex=.75,horizontal=T,las=1,xlab="Log_10 $")
 dev.off2()
 
-## Bar Plot: Number of Properties and Owners by Treatment
-pdf2(paste0(rand_path,"number_prop_own_by_trt_bar.pdf"))
-data[,.(.N,uniqueN(owner1)),by=tmt
-     ][,barplot(c(rbind(N,V2)),names.arg=rep(tmt,each=2),las=3,
+## Log Balance by Envelope
+pdf2(paste0(rand_path,"dist_log_due_by_trt_2_box.pdf"))
+par(mar=c(7.1,6.1,6.1,2.1))
+boxplot(log10(total_due)~bs,data=data_own,
+        main="Box Plots of Log Debt\nBy Treatment",
+        col=c("blue","red"),notch=T,boxwex=.25,
+        horizontal=T,las=1,xlab="Log_10 $")
+dev.off2()
+
+## Bar Plot: Number of Properties and Owners by Letter
+pdf2(paste0(rand_path,"number_prop_own_by_trt_7_bar.pdf"))
+par(mar=c(5.1,4.1,4.1,2.1))
+data[,.(.N,uniqueN(owner1)),by=tmt7
+     ][,barplot(c(rbind(N,V2)),names.arg=rep(tmt7,each=2),las=3,
                 main="Numbers of Properties and Owners\nBy Treatment",
                 col=rep(c("yellow","blue","darkgreen","red",
-                          "cyan","orange","orchid"),each=4),
+                          "cyan","orange","orchid"),each=2),
+                density=rep(c(-1,20),.N),cex.names=.75,
+                ylim=c(0,1.3*max(N)))]
+legend("top",legend=c("# Properties","# Owners"),
+       density=c(-1,20),bty="n",horiz=T,text.width=5)
+dev.off2()
+
+## Bar Plot: Number of Properties and Owners by Envelope
+pdf2(paste0(rand_path,"number_prop_own_by_trt_2_bar.pdf"))
+par(mar=c(5.1,4.1,4.1,2.1))
+data[,.(.N,uniqueN(owner1)),by=bs
+     ][,barplot(c(rbind(N,V2)),names.arg=rep(bs,each=2),las=3,
+                main="Numbers of Properties and Owners\nBy Treatment",
+                col=rep(c("blue","red"),each=2),
                 density=rep(c(-1,20),.N),
                 ylim=c(0,1.3*max(N)))]
-legend("topleft",legend=c("# Properties","# Owners"),density=c(-1,20),bty="n")
+legend("top",legend=c("# Properties","# Owners"),
+       density=c(-1,20),bty="n",horiz=T,text.width=2)
 dev.off2()
-
-## Bar Plot: Number of Properties and Owners by Treatment (among Unique Owners)
-pdf2(paste0(rand_path,"number_prop_own_by_trt_unq_own_bar.pdf"))
-data[opa_no %in% data[,if (.N==1) opa_no,by=owner1]$V1,.N,,by=tmt
-     ][,barplot(N,names.arg=tmt,las=3,
-                main="Numbers of Properties\nBy Treatment, Unique Owners",
-                col=rep(c("yellow","blue","darkgreen","red",
-                          "cyan","orange","orchid"),each=2))]
-dev.off2()
-
-## Map: Number of Properties by Neighborhood by Treatment
-phila_azav<-
-  readShapePoly("/media/data_drive/gis_data/PA/Neighborhoods_Philadelphia_with_quadrants.shp")
-
-phila_azav<-phila_azav[phila_azav$MAPNAME %in% data[,unique(azavea_nbhd)],]
-
-data[,n_tmt:=.N,by=tmt]
-data[,pct_nbhd:=.N/nrow(data),by=azavea_nbhd]
-data[,pct_nbhd_tmt:=.N/n_tmt,by=.(tmt,azavea_nbhd)]
-
-nbhd_data<-dcast(unique(data[,.(azavea_nbhd,tmt,pct_nbhd_tmt)]),
-                 azavea_nbhd~tmt,value.var="pct_nbhd_tmt")
-setkey(nbhd_data[,names(nbhd_data):=
-                   lapply(.SD,function(x)ifelse(is.na(x),0,x))],azavea_nbhd)
-nbhd_data[setkey(data,azavea_nbhd),full:=i.pct_nbhd]
-mx<-max(nbhd_data[,!"azavea_nbhd",with=F])
-upd<-setdiff(names(nbhd_data),"azavea_nbhd")
-nbhd_data[,(upd):=lapply(.SD,function(x)x/mx),.SDcols=upd]
-setcolorder(nbhd_data,c("azavea_nbhd","full",sort(unique(data$tmt))))
-
-setkey(nbhd_data[setkey(as.data.table(phila_azav@data)[,I:=.I],MAPNAME),
-                 ord:=i.I],ord)
-
-for (cols in upd){
-  pdf2(paste0(rand_path,"properties_by_nbhd_in_",cols,"_treatment_map.pdf"))
-  plot(phila_azav,col=gray(1-nbhd_data[[cols]]),
-       main=paste0("Distribution of Properties in ",cols))
-  dev.off2()
-}
-
-## Map: Number of Owners by Neighborhood by Treatment
-own_data<-unique(data,by="owner1")
-own_data[,n_tmt:=.N,by=tmt]
-own_data[,pct_nbhd:=.N/nrow(own_data),by=azavea_nbhd]
-own_data[,pct_nbhd_tmt:=.N/n_tmt,by=.(tmt,azavea_nbhd)]
-
-nbhd_own_data<-dcast(unique(own_data[,.(azavea_nbhd,tmt,pct_nbhd_tmt)],by=NULL),
-                     azavea_nbhd~tmt,value.var="pct_nbhd_tmt")
-setkey(nbhd_own_data[,names(nbhd_own_data):=
-                       lapply(.SD,function(x)ifelse(is.na(x),0,x))],azavea_nbhd)
-nbhd_own_data[data,full:=i.pct_nbhd]
-mx<-max(nbhd_own_data[,!"azavea_nbhd",with=F])
-nbhd_own_data[,(upd):=lapply(.SD,function(x)x/mx),.SDcols=upd]
-setcolorder(nbhd_own_data,c("azavea_nbhd","full",sort(unique(own_data$tmt))))
-
-setkey(nbhd_own_data[setkey(as.data.table(phila_azav@data)[,I:=.I],MAPNAME),
-                 ord:=i.I],ord)
-
-for (cols in upd){
-  pdf2(paste0(rand_path,"owners_by_nbhd_in_",cols,"_treatment_map.pdf"))
-  plot(phila_azav,col=gray(1-nbhd_own_data[[cols]]),
-       main=paste0("Distribution of Properties in ",cols))
-  dev.off2()
-}
