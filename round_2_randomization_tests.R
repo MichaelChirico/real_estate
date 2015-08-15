a662c7edc94b4508f0d7d3e677143400537f8b37
Michael Chirico <MichaelChirico4@gmail.com>
2015-08-15 16:08:17 -0400

    Closes #7

--- round_2_randomization_tests.R
+++ round_2_randomization_tests.R
@@ -22,6 +22,9 @@ dev.off2<-function(){
   invisible(dev.off(which=dev.list()["pdf"]))
 }
 
+#Shorthand for string concatenation
+"%+%"<-function(s1,s2)paste0(s1,s2)
+
 # Data import ####
 
 data<-fread("./round_two/round_2_full_data.csv")
@@ -36,10 +39,10 @@ data[,tmt:=paste0(substr(treatment,1,1),
 data_own<-data[,.(tmt=tmt[1],bs=bs[1],tmt7=tmt7[1],
                   total_due=sum(total_due)),by=owner1]
 
-# Graphic Tests
+# Graphic Tests ####
 rand_path<-"./papers_presentations/round_two/images/balance/"
 ## Log Balance by Letter
-pdf2(paste0(rand_path,"dist_log_due_by_trt_7_box.pdf"))
+pdf2(rand_path%+%"dist_log_due_by_trt_7_box.pdf")
 par(mar=c(7.1,6.1,6.1,2.1))
 boxplot(log10(total_due)~tmt7,data=data_own,
         main="Box Plots of Log Debt\nBy Treatment",
@@ -49,7 +52,7 @@ boxplot(log10(total_due)~tmt7,data=data_own,
 dev.off2()
 
 ## Log Balance by Envelope
-pdf2(paste0(rand_path,"dist_log_due_by_trt_2_box.pdf"))
+pdf2(rand_path%+%"dist_log_due_by_trt_2_box.pdf")
 par(mar=c(7.1,6.1,6.1,2.1))
 boxplot(log10(total_due)~bs,data=data_own,
         main="Box Plots of Log Debt\nBy Treatment",
@@ -58,28 +61,36 @@ boxplot(log10(total_due)~bs,data=data_own,
 dev.off2()
 
 ## Bar Plot: Number of Properties and Owners by Letter
-pdf2(paste0(rand_path,"number_prop_own_by_trt_7_bar.pdf"))
-par(mar=c(5.1,4.1,4.1,2.1))
 data[,.(.N,uniqueN(owner1)),by=tmt7
-     ][,barplot(c(rbind(N,V2)),names.arg=rep(tmt7,each=2),las=3,
-                main="Numbers of Properties and Owners\nBy Treatment",
-                col=rep(c("yellow","blue","darkgreen","red",
-                          "cyan","orange","orchid"),each=2),
-                density=rep(c(-1,20),.N),cex.names=.75,
-                ylim=c(0,1.3*max(N)))]
-legend("top",legend=c("# Properties","# Owners"),
-       density=c(-1,20),bty="n",horiz=T,text.width=5)
-dev.off2()
+     ][,{pdf2(rand_path%+%"number_prop_own_by_trt_7_bar.pdf")
+       layout(mat=matrix(1:2),heights=c(.8,.2))
+       par(mar=c(1,4.1,4.1,2.1))
+       x<-barplot(c(rbind(N,V2)),names.arg=rep(tmt7,each=2),las=3,
+                  main="Numbers of Properties and Owners\nBy Treatment",
+                  col=rep(c("yellow","blue","darkgreen","red",
+                            "cyan","orange","orchid"),each=2),
+                  density=rep(c(-1,20),.N),cex.names=.75,
+                  ylim=c(0,1.3*max(N)))
+       par(mar=rep(0,4))
+       plot(0,0,type="n",ann=F,axes=F,xlim=range(x))
+       legend(max(x)/2,0,bty="n",xjust=.3,
+              legend=c("# Properties","# Owners"),
+              density=c(-1,20),horiz=T,text.width=5)
+       dev.off2()}]
 
 ## Bar Plot: Number of Properties and Owners by Envelope
-pdf2(paste0(rand_path,"number_prop_own_by_trt_2_bar.pdf"))
-par(mar=c(5.1,4.1,4.1,2.1))
 data[,.(.N,uniqueN(owner1)),by=bs
-     ][,barplot(c(rbind(N,V2)),names.arg=rep(bs,each=2),las=3,
-                main="Numbers of Properties and Owners\nBy Treatment",
-                col=rep(c("blue","red"),each=2),
-                density=rep(c(-1,20),.N),
-                ylim=c(0,1.3*max(N)))]
-legend("top",legend=c("# Properties","# Owners"),
-       density=c(-1,20),bty="n",horiz=T,text.width=2)
-dev.off2()
+     ][,{pdf2(rand_path%+%"number_prop_own_by_trt_2_bar.pdf")
+       layout(mat=matrix(1:2),heights=c(.8,.2))
+       par(mar=c(1,4.1,4.1,2.1))
+       x<-barplot(c(rbind(N,V2)),names.arg=rep(bs,each=2),las=3,
+                  main="Numbers of Properties and Owners\nBy Treatment",
+                  col=rep(c("blue","red"),each=2),
+                  density=rep(c(-1,20),.N),
+                  ylim=c(0,1.3*max(N)))
+       par(mar=rep(0,4))
+       plot(0,0,type="n",ann=F,axes=F,xlim=range(x))
+       legend(max(x)/2,0,bty="n",xjust=.3,
+              legend=c("# Properties","# Owners"),
+              density=c(-1,20),horiz=T,text.width=1)
+       dev.off2()}]
