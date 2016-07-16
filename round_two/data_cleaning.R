4dad036ddddbdf8b240a0ca87253a6c4359bead1
Michael Chirico <MichaelChirico4@gmail.com>
2016-07-15 20:53:20 -0400

    final pass-through, mainly touching up comments for publication-readiness

--- round_two/data_cleaning.R
+++ round_two/data_cleaning.R
@@ -13,28 +13,21 @@ library(data.table)
 library(readxl)
 library(xlsx)
 setwd(mn <- "~/Desktop/research/Sieg_LMI_Real_Estate_Delinquency/")
-wds<-c(data = (dwd <- "/media/data_drive/") %+% "real_estate/",
-       proj = mn %+% "round_two/", log = mn %+% "logs/round_two/",
-       imga = mn %+% "round_two/images/analysis/",
-       imgb = mn %+% "round_two/images/balance/",
-       gis = dwd %+% "gis_data/PA/",
-       sher = dwd %+% "real_estate/sheriffs_sales/",
-       cens = dwd %+% "census/")
+wds <- c(data = "/media/data_drive/real_estate/",
+         proj = mn %+% "round_two/", 
+         log = mn %+% "logs/round_two/")
 write.packages(wds["log"] %+% "cleaning_session.txt")
 
-#"metavariables"
+#Specifying order/reference treatments for
+#  the 7- and 8-treatment groups
+#  (vs. Control and Holdout, resp.)
 trt.nms <- c("Control", "Amenities", "Moral", 
              "Duty", "Peer", "Lien", "Sheriff")
 trt.nms8 <- c("Holdout", trt.nms)
 
-mos <- c("jul", "sep", "dec")
-
 #Data Import ####
 ##Block I: Main Sample, July Cross-Section
-### total_due is pre-study balance
-### current_balance is as of July 22, 2015
-### total_paid is the accrual between June 1, 2015
-###   and July 22, 2015
+## * total_paid is the accrual between June 1, 2015 and July 22, 2015
 main_jul <- read.xlsx3(
   wds["data"] %+% "Payments and Balance Penn Letter Experiment_150727.xlsx",
   colIndex = c(2L, 5L, 8L, 9L, 15L),
@@ -68,9 +61,9 @@ holdout_jul[ , treat15 := "Holdout"]
 
 update_opas <- data.table(old = "057027304",
                           new = "057026500")
-holdout_jul[update_opas, opa_no := i.old, on = c("opa_no" = "new")]
+holdout_jul[update_opas, opa_no := i.old, on = c(opa_no = "new")]
 
-##Block III: Combined Sample, September Cross-Section
+##Block III: Full Sample, September Cross-Section
 full_sep <- read_excel(
   wds["data"] %+%
     "req20150709_PennLetterExperiment (September 2015 update) v2.xlsx",
@@ -85,15 +78,12 @@ full_sep <- read_excel(
 
 setDT(full_sep)
 
-###The following accounts changed OPA #s between samples,
-###  See correspondence with Darryl Watson dated:
-###    Tue, Sep 15, 2015 at 4:09 PM
-###  (should also be able to corroborate with Account ID)
+###  **TO DO: INSERT E-MAIL META DATA FROM DOR CONFIRMATION**
 update_opas <- data.table(old = c("151102600", "884350465"),
                           new = c("151102610", "881577275"))
 full_sep[update_opas, opa_no := i.old, on = c("opa_no" = "new")]
 
-##Block IV: Combined Sample, December Cross-Section
+##Block IV: Full Sample, December Cross-Section
 full_dec <- read_excel(
   wds["data"] %+% "req20150709_PennLetterExperiment (December 2015 update) v2.xlsx",
   #See above about length-2 NA
@@ -105,8 +95,6 @@ full_dec <- read_excel(
 
 setDT(full_dec)
 
-###In addition to the two from the three-month sample,
-###  two more properties have updated OPAs.
 ###  **TO DO: INSERT E-MAIL META DATA FROM DOR CONFIRMATION**
 update_opas <-
   data.table(old = c("151102600", "884350465", "882932475", "213155700"),
@@ -114,6 +102,7 @@ update_opas <-
 full_dec[update_opas, opa_no := i.old, on = c("opa_no" = "new")]
 
 ##Block V: Main Sample Background Data
+## * total_due is pre-study balance
 main_bg <- fread(wds["proj"] %+% "round_2_full_data.csv", 
                  select = c("opa_no", "owner1", "total_due", "rand_id"))
 
@@ -131,14 +120,18 @@ setnames(opa_bg, c("opa_no", "assessed_mv"))
 ### Framework:
 ###  I  III IV V  VII
 ###  II III IV VI VII
+### (blocks stacked vertically will be stitched/appended,
+###  and each column involves a merge to the previous column)
 bgvars <- c("opa_no", "owner1", "total_due", "rand_id")
-
 properties <-
   rbind(main_jul, holdout_jul
         )[full_sep, on = "opa_no"
           ][full_dec, on = "opa_no"
+            #Need fill since holdout_bg lacks rand_id
             ][rbind(main_bg, holdout_bg, fill = TRUE),
               (bgvars) := mget("i." %+% bgvars), on = "opa_no"
+              #one of the Excel files is funky and the reader
+              #  added a phantom row; this removes that
               ][!is.na(treat15)]
 
 properties[opa_bg, assessed_mv := as.numeric(i.assessed_mv), on = "opa_no"]
@@ -153,29 +146,25 @@ inds <- c("paid_full_jul", "ever_paid_jul",
 properties[ , (inds) := lapply(.SD, function(x) x == "Y"), .SDcols = inds]
 ###Paid Full actually stored opposite because 
 ###  question in data is: "Does property have a balance?"
-pf <- "paid_full_" %+% mos
+pf <- "paid_full_" %+% c("jul", "sep", "dec")
 properties[ , (pf) := lapply(.SD, `!`), .SDcols = pf]
 
 ##Define some flags
 ### Is this a holdout property?
 properties[ , holdout := treat15 == "Holdout"]
-### Did this owner receive more than one letter?
-properties[ , flag_multiple_property := .N > 1L, by = owner1]
 ### Was this owner in both the holdout sample and the treatment panel?
 properties[ , flag_holdout_overlap := any(treat15 == "Holdout") &&
               any(treat15 != "Holdout"), by = owner1]
-
-properties[(!holdout), treat14 := gsub("\\sE.*", "", gsub("_", " ", treat15))]
-properties[ , treat14 := 
-              factor(treat14, 
-                     paste(rep(trt.nms, each = 2L), c("Small", "Big")))]
 
 properties[ , treat8 := factor(gsub("_.*", "", treat15), trt.nms8)]
 properties[(!holdout), treat7 := factor(treat8)]
 
 ###Get owner-level version of data, keeping only key analysis variables
 owners <- 
-  properties[order(treat14),
+  #Sort by treat7 within owner1 so that Holdout properties sink to
+  #  the bottom (forcing the treatment associated with each
+  #  owner to be that of their regular treatment group, if any)
+  properties[order(treat7),
              .(treat7 = treat7[1L],
                treat8 = treat8[1L],
                rand_id = rand_id[1L],
